d2eb68ef15325cc35cb9113ed223c4fbc83c49d2
Paul Young <p.young79@outlook.com>
2017-02-21 20:31:12 +0000

    make the timer stuff a simple library

---
+++ lib/timer/timer.h
@@ -0,0 +1,7 @@
+#ifndef _TIMER_H_
+#define _TIMER_H_
+
+void timer_init();
+bool timer_getTrigger();
+
+#endif //_TIMER_H_
--- src/Blink_SIL.cpp
+++ src/Blink_SIL.cpp
@@ -14,62 +14,30 @@
 // this code will enable one arduino timer interrupt.
 // timer1 will interrupt at 1Hz
 #include <Arduino.h>
+#include "timer.h"
 
-static volatile boolean timer_trigger;
-static boolean led_state;
-
-void init_timer()
-{
-	noInterrupts(); // stop interrupts
-
-	// set timer1 interrupt at 1Hz
-	TCCR1A = 0; // set entire TCCR1A register to 0
-	TCCR1B = 0; // same for TCCR1B
-	TCNT1  = 0; // initialize counter value to 0
-
-	// set compare match register for 1hz increments
-	//OCR1A = 15624; // = (16*10^6) / (1*1024) - 1 (must be <65536)
-	OCR1A = 1952; // = (16*10^6) / (1*1024) - 1 (must be <65536)
-
-	// turn on CTC mode
-	TCCR1B |= (1 << WGM12);
-
-	// Set CS12 and CS10 bits for 1024 prescaler
-	TCCR1B |= (1 << CS12) | (1 << CS10);
-
-	// enable timer compare interrupt
-	TIMSK1 |= (1 << OCIE1A);
-
-	interrupts(); // allow interrupts
-}
+static bool led_state;
 
 void setup()
 {
 	// initialise variables
-	timer_trigger = false;
 	led_state = HIGH;
 
 	// set pins as outputs
 	pinMode(LED_BUILTIN, OUTPUT);
 
 	// initialise the timer module
-	init_timer();
+	timer_init();
 } // end setup
-
-ISR(TIMER1_COMPA_vect)
-{
-	// timer1 interrupt 1Hz toggles pin 13 (LED)
-	// generates pulse wave of frequency 1Hz/2 = 0.5kHz (takes two cycles for full wave- toggle high then toggle low)
-	timer_trigger = true;
-}
 
 void loop()
 {
-	// do other things here
-	if(timer_trigger)
-	{
-		timer_trigger = false;
+	bool trigger;
 
+	// do other things here
+	trigger = timer_getTrigger();
+	if(trigger)
+	{
 		led_state ^= 1;
 		digitalWrite(LED_BUILTIN, led_state);
 	}
